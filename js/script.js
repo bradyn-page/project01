6756f49de5e7dc3b2213c6ac03365f553dd1dad3
bradyn-page <bradynanderson1@gmail.com>
2025-02-09 12:25:03 -0600

    slider and contact
    started contact form, finished slider

--- js/script.js
+++ js/script.js
@@ -6,11 +6,12 @@ let header = document.querySelector('.header');
 header.classList.toggle('sticky', window.scrollY > 100);
 };
 
-/*===== swiper ========*/
+/*===== swiper from swiperjs.com ========*/
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
-    spaceBetween: 30,
+    spaceBetween: 50,
     loop: true,
+    grabCursor: true, //make cursor grabby on testimonials
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
